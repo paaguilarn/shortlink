cfb55370e4b3039186038303039b28a04b3a75bc
Pedro Aguilar <pedroaguilaroficial@gmail.com>
2024-04-01 00:20:29 -0600

    [STYLE] minor black style corrections

--- core/config.py
+++ core/config.py
@@ -10,8 +10,8 @@ class Settings(BaseSettings):
     project_prefix: str = "shortlink"
     api_v1_route: str = f"/api/v1/{project_prefix}"
     openapi_route: str = f"/api/v1/{project_prefix}/openapi.json"
-    docs_url: str = f'/docs/{project_prefix}'
-    redoc_url: str = f'/redoc/{project_prefix}'
+    docs_url: str = f"/docs/{project_prefix}"
+    redoc_url: str = f"/redoc/{project_prefix}"
 
     short_url_len: int = Field(8, alias="SHORT_URL_LEN")
 
